92748068d125bac3ddc55f59bba5e75081e69643
Olivia Uesugui <uesugui.li@gmail.com>
2022-11-24 02:11:35 -0300

    :sparkles: feat: busca dos dados da API utilizando axios

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,9 +3,12 @@ import appPreviewImg from "../assets/app-nlw-copa-preview.png";
 import logoImg from "../assets/logo.svg";
 import usersAvatarExampleImg from "../assets/users-avatar-example.png";
 import iconCheckImg from "../assets/icon-check.svg";
+import { api } from "../lib/axios";
 
 interface HomeProps {
   poolCount: number;
+  guessCount: number;
+  userCount: number;
 }
 
 export default function Home(props: HomeProps) {
@@ -20,8 +23,8 @@ export default function Home(props: HomeProps) {
         <div className="mt-10 flex items-center gap-2">
           <Image src={usersAvatarExampleImg} alt="" />
           <strong className="text-gray-100 text-xl">
-            <span className="text-ignite-500">+12.592</span> pessoas já estão
-            usando
+            <span className="text-ignite-500">+{props.userCount}</span> pessoas
+            já estão usando
           </strong>
         </div>
         <form className="mt-10 flex gap-2">
@@ -54,7 +57,7 @@ export default function Home(props: HomeProps) {
           <div className="flex items-center gap-6">
             <Image src={iconCheckImg} alt=""></Image>
             <div className="flex flex-col">
-              <span className="font-bold text-2xl">+2.034</span>
+              <span className="font-bold text-2xl">+{props.guessCount}</span>
               <span>Palpites enviados</span>
             </div>
           </div>
@@ -70,12 +73,17 @@ export default function Home(props: HomeProps) {
 }
 
 export const getServerSideProps = async () => {
-  const response = await fetch("http://localhost:3333/pools/count");
-  const data = await response.json();
-  console.log(data);
+  const [poolCountResponse, guessCountResponse, userCountResponse] =
+    await Promise.all([
+      api.get("/pools/count"),
+      api.get("/guesses/count"),
+      api.get("/users/count"),
+    ]);
   return {
     props: {
-      poolCount: data.count,
+      poolCount: poolCountResponse.data.count,
+      guessCount: guessCountResponse.data.count,
+      userCount: userCountResponse.data.count,
     },
   };
 };
